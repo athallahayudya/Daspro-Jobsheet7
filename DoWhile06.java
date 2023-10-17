af1d709bfb867d5861d231f6700cbc0a2d425d11
Athallah Ayudya <74366958+athallahayudya@users.noreply.github.com>
2023-10-17 10:26:23 +0700

    Add files via upload

---
+++ DoWhile06.java
@@ -0,0 +1,11 @@
+public class DoWhile06 {
+
+    public static void main(String[] args) {
+        int x = 0;
+        do {
+            System.out.println(x);
+        } while (++x <= 8);
+        System.out.println("Program berhenti");
+
+    }
+}
