64855243a520519552e521c4921b2abedc231cb6
Athallah Ayudya <74366958+athallahayudya@users.noreply.github.com>
2023-10-17 10:39:25 +0700

    Add files via upload

---
+++ For06.java
@@ -0,0 +1,11 @@
+/**
+ * For06
+ */
+public class For06 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++ ) {
+            System.out.println("Hasil kuadrat dari " + i + " adalah " + (i * i));
+        }
+    }
+}
---
+++ ForVar106.java
@@ -0,0 +1,12 @@
+/**
+ * ForVar106
+ */
+public class ForVar106 {
+
+    public static void main(String[] args) {
+        
+        for (int i = 1, j = 10; i < j; j-- ) {
+            System.out.printf("%03d -- %03d/n", i, j);
+        }
+    }
+}
